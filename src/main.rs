85d60fc0d4c6f7de8338a74076775196d0a85984
heydabop <heydabop@gmail.com>
2020-05-10 12:34:17 -0500

    read logfile into discord

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ extern crate ctrlc;
 
 use serenity::client::Client;
 use serenity::framework::standard::{macros::group, StandardFramework};
+use serenity::http::Http;
+use serenity::model::id::ChannelId;
+use std::error::Error;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom};
+use std::sync::Arc;
+use std::thread;
 
 #[group]
 struct Terraria;
@@ -18,17 +25,23 @@ fn main() {
         .as_str()
         .expect("config.toml missing bot_token");
 
+    let server_logfile = config["server_logfile"]
+        .as_str()
+        .expect("config.toml missing server_logfile");
+
+    let bridge_channel_id = config["bridge_channel_id"]
+        .as_str()
+        .expect("config.toml missing bridge_channel_id")
+        .parse::<u64>()
+        .expect("bridge_channel_id not u64");
+
     let client_handler = handler::Handler {
         playing: config["server_url"]
             .as_str()
             .or(Some(""))
             .unwrap()
             .to_string(),
-        bridge_channel_id: config["bridge_channel_id"]
-            .as_str()
-            .expect("config.toml missing bridge_channel_id")
-            .parse::<u64>()
-            .expect("bridge_channel_id not u64"),
+        bridge_channel_id,
     };
 
     let mut client = Client::new(bot_token, client_handler).expect("Error creating client");
@@ -38,15 +51,52 @@ fn main() {
             .group(&TERRARIA_GROUP),
     );
 
-    let manager = client.shard_manager.clone();
+    let shutdown_manager = client.shard_manager.clone();
 
     if let Err(e) = ctrlc::set_handler(move || {
-        manager.lock().shutdown_all();
+        shutdown_manager.lock().shutdown_all();
     }) {
         eprintln!("Error setting SIGINT handler: {}", e);
     }
 
+    send_loglines(
+        server_logfile,
+        client.cache_and_http.http.clone(),
+        ChannelId(bridge_channel_id),
+    )
+    .expect("Unable to start log file thread");
+
     if let Err(e) = client.start() {
         eprintln!("An error occurred while running the client: {:?}", e);
     }
+}
+
+fn send_loglines(
+    filename: &str,
+    http: Arc<Http>,
+    channel_id: ChannelId,
+) -> Result<(), Box<dyn Error>> {
+    let mut file = File::open(filename)?;
+    let mut pos = file.metadata()?.len();
+
+    thread::spawn(move || {
+        file.seek(SeekFrom::Start(pos))
+            .expect("Unable to seek to end of logfile");
+        loop {
+            let mut line = String::new();
+            let bytes = file
+                .read_to_string(&mut line)
+                .expect("Unable to read line from logfile");
+            if !line.is_empty() {
+                if let Err(e) = channel_id.say(&http, line) {
+                    eprintln!("Unable to send logline to discord: {}", e);
+                }
+            }
+            pos += bytes as u64;
+            file.seek(SeekFrom::Start(pos))
+                .expect("Unable to re-seek to end of logfile");
+        }
+    });
+
+    Ok(())
 }
