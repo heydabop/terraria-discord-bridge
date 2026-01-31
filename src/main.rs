54dc9a090ca6129b53e77440417649fa20dfe416
heydabop <heydabop@gmail.com>
2026-01-31 11:04:04 -0600

    break commands into own file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod commands;
 mod strings;
 mod terraria_pcap;
 
@@ -11,15 +12,12 @@ use serenity::model::id::ChannelId;
 use serenity::prelude::*;
 use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
 use sqlx::{ConnectOptions, Pool, Postgres};
-use std::collections::HashMap;
-use std::fmt::Write as _;
 use std::fs;
 use std::process::{Stdio, exit};
 use std::sync::Arc;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 use tokio::signal::unix::{SignalKind, signal};
-use tokio::time::{Duration, sleep};
 use tracing::{error, info};
 
 struct Data {
@@ -27,8 +25,6 @@ struct Data {
     admin_user_id: UserId,
     server_dir: String,
 }
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[derive(Deserialize)]
 struct Config {
@@ -105,7 +101,12 @@ async fn main() {
     let data_db = db_pool.clone();
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![deaths(), playing(), update(), version()],
+            commands: vec![
+                commands::deaths(),
+                commands::playing(),
+                commands::update(),
+                commands::version(),
+            ],
             ..Default::default()
         })
         .setup(move |ctx, _ready, framework| {
@@ -160,216 +161,6 @@ async fn main() {
     if let Err(e) = client.start().await {
         error!(error = %e, "An error occurred while running the client");
     }
-}
-
-/// Show players sorted by how many times they've died
-#[poise::command(slash_command, prefix_command)]
-async fn deaths(ctx: Context<'_>) -> Result<(), Error> {
-    let data = ctx.data();
-    #[allow(clippy::expect_used)]
-    let db = &data.db;
-
-    #[allow(clippy::panic)]
-    match sqlx::query!("SELECT victim FROM death").fetch_all(db).await {
-        Err(e) => Err(format!("Unable to query deaths: {e}").into()),
-        Ok(rows) => {
-            let mut death_map: HashMap<String, u32> = HashMap::new();
-            for row in rows {
-                let victim: String = row.victim;
-                match death_map.get(&victim) {
-                    None => death_map.insert(victim, 1),
-                    Some(&victim_deaths) => death_map.insert(victim, victim_deaths + 1),
-                };
-            }
-            let mut deaths = vec![];
-            for (victim, victim_deaths) in &death_map {
-                deaths.push((victim, victim_deaths));
-            }
-            deaths.sort_by(|a, b| b.1.cmp(a.1));
-
-            let mut content = String::new();
-            for death in deaths {
-                writeln!(content, "{} - {}", death.0, death.1)?;
-            }
-            if content.is_empty() {
-                writeln!(content, "No deaths, yet...")?;
-            }
-
-            if let Err(e) = ctx.say(content).await {
-                return Err(format!("Error replying to deaths command: {e}").into());
-            }
-
-            Ok(())
-        }
-    }
-}
-
-/// Show who's currently online
-#[poise::command(slash_command, prefix_command)]
-async fn playing(ctx: Context<'_>) -> Result<(), Error> {
-    if !Command::new("tmux")
-        .args(["send-keys", "-t", "terraria", "playing", "Enter"])
-        .output()
-        .await?
-        .status
-        .success()
-    {
-        ctx.say("Command failed").await?;
-        return Ok(());
-    }
-
-    ctx.say("Sending...").await?;
-
-    Ok(())
-}
-
-/// Show current server version
-#[poise::command(slash_command, prefix_command)]
-async fn version(ctx: Context<'_>) -> Result<(), Error> {
-    if !Command::new("tmux")
-        .args(["send-keys", "-t", "terraria", "version", "Enter"])
-        .output()
-        .await?
-        .status
-        .success()
-    {
-        ctx.say("Command failed").await?;
-        return Ok(());
-    }
-
-    ctx.say("Sending...").await?;
-
-    Ok(())
-}
-
-/// Update server from old version to new version
-#[poise::command(slash_command, prefix_command)]
-async fn update(
-    ctx: Context<'_>,
-    #[description = "Current server version"] old_version: String,
-    #[description = "New version to update to"] new_version: String,
-) -> Result<(), Error> {
-    // only allow admin to invocate command
-    let author_id = ctx.author().id;
-    let admin_user_id = ctx.data().admin_user_id;
-    if author_id != admin_user_id {
-        ctx.say(format!("I only listen to <@{admin_user_id}>"))
-            .await?;
-        return Ok(());
-    }
-
-    ctx.defer().await?;
-
-    let server_dir = ctx.data().server_dir.clone();
-    let zipfile = format!("terraria-server-{new_version}.zip");
-
-    // download new zip
-    if !Command::new("curl")
-        .current_dir(&server_dir)
-        .args([
-            "-o",
-            &zipfile,
-            &format!("https://terraria.org/api/download/pc-dedicated-server/{zipfile}",),
-        ])
-        .output()
-        .await?
-        .status
-        .success()
-    {
-        ctx.say("Unable to download server zip").await?;
-        return Ok(());
-    }
-
-    // unzip new server
-    if !Command::new("unzip")
-        .current_dir(&server_dir)
-        .args([zipfile])
-        .output()
-        .await?
-        .status
-        .success()
-    {
-        ctx.say("Unable to unzip server").await?;
-        return Ok(());
-    }
-
-    // chmod new binary
-    if !Command::new("chmod")
-        .current_dir(&server_dir)
-        .args([
-            String::from("u+x"),
-            format!("{new_version}/Linux/TerrariaServer.bin.x86_64"),
-        ])
-        .output()
-        .await?
-        .status
-        .success()
-    {
-        ctx.say("Unable to chmod server binary").await?;
-        return Ok(());
-    }
-
-    // check if server is running
-    if Command::new("pgrep")
-        .args(["-f", "TerrariaServer.bin.x86_64"])
-        .output()
-        .await?
-        .status
-        .success()
-    {
-        // exit server if so
-        Command::new("tmux")
-            .args(["send-keys", "-t", "terraria", "exit", "Enter"])
-            .output()
-            .await?;
-
-        sleep(Duration::from_secs(5)).await;
-
-        // check if server is still running
-        if Command::new("pgrep")
-            .args(["-f", "TerrariaServer.bin.x86_64"])
-            .output()
-            .await?
-            .status
-            .success()
-        {
-            ctx.say("Unable to stop server").await?;
-            return Ok(());
-        }
-    }
-
-    // replace directory in start_server.sh
-    if !Command::new("sed")
-        .current_dir(&server_dir)
-        .args([
-            "-i",
-            &format!("s/{old_version}/{new_version}/g"),
-            "start_server.sh",
-        ])
-        .output()
-        .await?
-        .status
-        .success()
-    {
-        ctx.say("Unable to sed server script").await?;
-        return Ok(());
-    }
-
-    // restart server
-    Command::new("tmux")
-        .args(["send-keys", "-t", "terraria", "./start_server.sh", "Enter"])
-        .output()
-        .await?;
-
-    sleep(Duration::from_secs(10)).await;
-
-    // check version
-    Command::new("tmux")
-        .args(["send-keys", "-t", "terraria", "version", "Enter"])
-        .output()
-        .await?;
-
-    Ok(())
 }
 
 // "tail"s server logfile, sending new lines to discord
